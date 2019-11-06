02b99ddb969ec9ddd8621311831e218734df5256
RAN VAKNIN <ranvaknin0@gmail.com>
2019-11-06 13:44:16 -0800

    map works

--- server.js
+++ server.js
@@ -1,104 +1,89 @@
 'use strict';
 
-// Load Enviornment Variables from the .env file
-require('dotenv').config()
+// Load Environment Variables from the .env file
+require('dotenv').config();
 
 // Application Dependencies
 const express = require('express');
 const superagent = require('superagent');
-const cors = require('cors')
+const cors = require('cors');
 
 // Application Setup
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 const app = express();
 app.use(cors());
 
 let locations = {};
 
-// Route Deffinitions
+// Route Definitions
 app.get('/location', locationHandler);
 app.get('/weather', weatherHandler);
 app.use('*', notFoundHandler);
 app.use(errorHandler);
 
-// app.get('/location', (request, response) => {
-//   const geoData = require('./data/geo.json');
-//   const city = request.query.data;
-//   if (geoData.results[0].address_components[0].long_name === city){
-//     const locationData = new Location(city,geoData);
-//     response.send(locationData);
-//   } else{
-//     response.send('Josh is amazeballs. ERROR 500!!!!!!!')
-//   }
-  
-// });
 
-// app.get('/weather', (req,res) =>{
-//   const darkSky = require('./data/darksky.json');
-//   const	city = req.query.data;
-//   let arrayOfDays = [];
-//   for (let i = 0 ; i < 8 ; i++){
-//     const summary = darkSky.daily.data[i].summary;
-//     const time = darkSky.daily.data[i].time;
-//     const weatherData = new Weather(city,summary,time);
-//     arrayOfDays.push(weatherData);
-//   }
-//   res.send(arrayOfDays);
-// })
+function locationHandler(request,response) {
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`;
 
-function locationHandler(request, response) {
-  
-  const url = `https://maps.googleapis.com/maps/api/geocode/json?`;
+  console.log('DATAAAA:',request.query.data);
 
   if ( locations[url] ) {
-    response.send( locations[url] );
+    response.send(locations[url]);
   }
   else {
     superagent.get(url)
       .then(data => {
         const geoData = data.body;
         const location = new Location(request.query.data, geoData);
-        location[url] = location;
+        locations[url] = location;
         response.send(location);
       })
       .catch( () => {
         errorHandler('So sorry, something went wrong.', request, response);
-      })
+      });
   }
 }
 
-function Location(city, geoData){
-  this.request_query = city;
+function Location(query, geoData) {
+  this.search_query = query;
   this.formatted_query = geoData.results[0].formatted_address;
   this.latitude = geoData.results[0].geometry.location.lat;
-  this.longtitude = geoData.results[0].geometry.location.lng;
+  this.longitude = geoData.results[0].geometry.location.lng;
 }
+
 
 // http://localhost:3000/weather?data%5Blatitude%5D=47.6062095&data%5Blongitude%5D=-122.3320708
 // That encoded query string is: data[latitude]=47.6062095&data[longitude]=122.3320708
-function weatherHandler(request, response) {
-  
-  const url = `https://api.darksky.net/forecast/`
+function weatherHandler(request,response) {
+
+  const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
 
   superagent.get(url)
     .then( data => {
       const weatherSummaries = data.body.daily.data.map(day => {
         return new Weather(day);
-      })
+      });
       response.status(200).json(weatherSummaries);
     })
     .catch( () => {
-      errorHandler('Somnething went wrong', request, response);
-    })
+      errorHandler('So sorry, something went wrong.', request, response);
+    });
+
 }
 
-function Weather(summary,time){
-  this.summary = day.summary;
-  this.time = new Date(day.time*1000).toGMTString();
+function Weather(day) {
+  this.forecast = day.summary;
+  this.time = new Date(day.time * 1000).toString().slice(0, 15);
 }
 
-function notFound
+function notFoundHandler(request,response) {
+  response.status(404).send('huh?');
+}
 
-app.listen(PORT, () => {
-  console.log(`listening on port: ${PORT}.`)
-});
+function errorHandler(error,request,response) {
+  response.status(500).send(error);
+}
+
+
+// Make sure the server is listening for requests
+app.listen(PORT, () => console.log(`App is listening on ${PORT}`) );
