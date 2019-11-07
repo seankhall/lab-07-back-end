79845a0b608c2a5e65e94ba45bb80831f8baba3a
RAN VAKNIN <ranvaknin0@gmail.com>
2019-11-07 00:34:53 -0800

    finished app for today

--- server.js
+++ server.js
@@ -18,16 +18,16 @@ let locations = {};
 // Route Definitions
 app.get('/location', locationHandler);
 app.get('/weather', weatherHandler);
+app.get('/yelp', yelpHandler);
+app.get('/trails', trailHandler);
 app.use('*', notFoundHandler);
 app.use(errorHandler);
 
-
-function locationHandler(request,response) {
+//handlers
+function locationHandler(request, response) {
   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`;
 
-  console.log('DATAAAA:',request.query.data);
-
-  if ( locations[url] ) {
+  if (locations[url]) {
     response.send(locations[url]);
   }
   else {
@@ -38,12 +38,78 @@ function locationHandler(request,response) {
         locations[url] = location;
         response.send(location);
       })
-      .catch( () => {
+      .catch(() => {
         errorHandler('So sorry, something went wrong.', request, response);
       });
   }
 }
 
+function yelpHandler(request, response) {
+  const url = `https://api.yelp.com/v3/businesses/search?latitude=${request.query.data.latitude}&longitude=${request.query.data.longitude}`
+
+  if (locations[url]) {
+    response.send(locations[url]);
+    console.log('if statment')
+  }
+  else {
+    superagent.get(url)
+      .set('Authorization', `Bearer ${process.env.YELP_API_KEY}`)
+      .then(data => {
+        const restaurant = data.body.businesses.map(business => {
+          return new Yelp(business)
+        })
+        response.status(200).json(restaurant)
+      })
+      .catch(() => {
+        errorHandler('So sorry, something went wrong.', request, response);
+        console.log('else after catch statment')
+      });
+  }
+}
+
+function weatherHandler(request, response) {
+  const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
+
+  superagent.get(url)
+    .then(data => {
+      const weatherSummaries = data.body.daily.data.map(day => {
+        return new Weather(day);
+      });
+      response.status(200).json(weatherSummaries);
+    })
+    .catch(() => {
+      errorHandler('So sorry, something went wrong.', request, response);
+    });
+}
+
+function trailHandler(request, response) {
+  const url = `https://www.hikingproject.com/data/get-trails?lat=${request.query.data.latitude}&lon=${request.query.data.longitude}&maxDistance=10&key=${process.env.HIKING_API}`
+  if (locations[url]) {
+    response.send(locations[url]);
+  }
+  else {
+    superagent.get(url)
+      .then(data => {
+        const allTrails = data.body.trails.map(trail => {
+          return new Trails(trail)
+        })
+        response.status(200).json(allTrails)
+      })
+      .catch(() => {
+        errorHandler('So sorry, something went wrong.', request, response);
+      });
+  }
+}
+
+function notFoundHandler(request, response) {
+  response.status(404).send('huh?');
+}
+
+function errorHandler(error, request, response) {
+  response.status(500).send(error);
+}
+
+//constructors for API
 function Location(query, geoData) {
   this.search_query = query;
   this.formatted_query = geoData.results[0].formatted_address;
@@ -51,24 +117,12 @@ function Location(query, geoData) {
   this.longitude = geoData.results[0].geometry.location.lng;
 }
 
-
-// http://localhost:3000/weather?data%5Blatitude%5D=47.6062095&data%5Blongitude%5D=-122.3320708
-// That encoded query string is: data[latitude]=47.6062095&data[longitude]=122.3320708
-function weatherHandler(request,response) {
-
-  const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
-
-  superagent.get(url)
-    .then( data => {
-      const weatherSummaries = data.body.daily.data.map(day => {
-        return new Weather(day);
-      });
-      response.status(200).json(weatherSummaries);
-    })
-    .catch( () => {
-      errorHandler('So sorry, something went wrong.', request, response);
-    });
-
+function Yelp(place) {
+  this.name = place.name;
+  this.image_url = place.image_url;
+  this.price = place.price;
+  this.rating = place.rating;
+  this.url = place.url;
 }
 
 function Weather(day) {
@@ -76,14 +130,18 @@ function Weather(day) {
   this.time = new Date(day.time * 1000).toString().slice(0, 15);
 }
 
-function notFoundHandler(request,response) {
-  response.status(404).send('huh?');
+function Trails(trail){
+  this.name = trail.name;
+  this.location = trail.location;
+  this.length = trail.length;
+  this.stars = trail.stars;
+  this.star_votes = trail.star_votes;
+  this.summary = trail.summary;
+  this.trail_url = trail.trail_url;
+  this.conditions = trail.conditions;
+  this.condition_date = trail.condition_date;
+  this.condition_time = trail.condition_time;
 }
-
-function errorHandler(error,request,response) {
-  response.status(500).send(error);
-}
-
 
 // Make sure the server is listening for requests
-app.listen(PORT, () => console.log(`App is listening on ${PORT}`) );
+app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
